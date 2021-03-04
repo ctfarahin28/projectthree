cf98c893952064f23a3abb7ba61424539c4ceee9
Rin <42369047+ctfarahin28@users.noreply.github.com>
2021-03-04 22:26:40 +0800

    home edit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    {{ __('You are logged in!') }}
+                    {{ __('You are logged in! Welcome to my website') }}
                 </div>
             </div>
         </div>
